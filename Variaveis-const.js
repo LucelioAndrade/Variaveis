1e09af9de53a62fdb2013120c7f6ccfddeb63d36
LucelioAndrade <lucelio.andrade@gmail.com>
2023-07-10 23:08:30 -0300

    Criação

---
+++ Variaveis-const.js
@@ -0,0 +1,9 @@
+// criar uma variavel e guardar um string
+
+let primeiroNome = "Lucelio";
+
+console.log(primeiroNome);
+
+primeiroNome = "Andrade";
+
+console.log(primeiroNome);
---
+++ Variaveis-number.js
@@ -0,0 +1,7 @@
+//criar uma variavel e guardar um number
+
+let idade = 46;
+console.log(idade);
+
+let auturaEmMetros = 1.80;
+console.log(auturaEmMetros);
